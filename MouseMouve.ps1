587b6b806b9140519424d3a155111c7442a92ff4
Alexandre <98089104+ALexandreGr17@users.noreply.github.com>
2022-12-01 22:41:02 +0100

    Update MouseMouve.ps1

--- MouseMouve.ps1
+++ MouseMouve.ps1
@@ -1,15 +1,16 @@
 $MYJOB = Start-Job -ScriptBlock {
 
 $MOVEMENTSIZE = 500
-$SLEEPTIME = 500
-
+$SLEEPTIME = 0.5
 Add-Type -AssemblyName System.Windows.Forms
 while ($true) {
 $POSITION = [Windows.Forms.Cursor]::Position
-$POSITION.x += ($MOVEMENTSIZE + Get-Random -Maximum 500 -Minimum -500)
-$POSITION.y += ($MOVEMENTSIZE + Get-Random -Maximum 500 -Minimum -500)
+$rx =  Get-Random -Maximum 500 -Minimum -500
+$ry =  Get-Random -Maximum 500 -Minimum -500
+$POSITION.x += ($MOVEMENTSIZE + $rx)
+$POSITION.y += ($MOVEMENTSIZE + $ry)
 [Windows.Forms.Cursor]::Position = $POSITION
-Start-Sleep -Milliseconds $SLEEPTIME
+Start-Sleep -Seconds $SLEEPTIME
 $POSITION = [Windows.Forms.Cursor]::Position
 $POSITION.x -= $MOVEMENTSIZE
 $POSITION.y -= $MOVEMENTSIZE
